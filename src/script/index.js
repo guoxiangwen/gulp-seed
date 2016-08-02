72026c9833c740ca91f3a86792de1b7e23b81ac7
guoxiangwen <gxw1990**>
2016-08-02 11:59:00 +0800

    。。。

--- src/script/index.js
+++ src/script/index.js
@@ -1,6 +1,5 @@
 !function () {
     $.ajax({
-        // url: 'http://cnodejs.org/api/v1/topics?tab=all&page=1&limit=10&mdrender=false',
         url: '/openrest/v1/open/ipp/product/categorys',
         type: 'get',
         success: function (data) {
